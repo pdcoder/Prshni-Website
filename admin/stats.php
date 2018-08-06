5cc5b3211ce41f8bf295a67efa7bbb5780dada03
Prakash Das <prakashdas1998.pd@gmail.com>
2018-08-06 22:30:13 +0530

    Updated stats.php

--- admin/stats.php
+++ admin/stats.php
@@ -22,48 +22,16 @@ if(isset($_POST['submit']))
         if ($result1 = $conn->query($sql1))
         {
             $error = "Successfully updated";
+            header("Refresh:0");
         }
         else
         $error = "There was some error.Please try again.";
     }
 }
-?><!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
+?>      
+<?php include("header.php");?>
     <title>Stats</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="./vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- DataTables CSS -->
-    <link href="./vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
-
-    <!-- DataTables Responsive CSS -->
-    <link href="./vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="./dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="./vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <style>
         .error{
             color:red;
@@ -92,57 +60,7 @@ input[type=number] {
     <div id="wrapper">
 
         <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                
-                <a class="navbar-brand" href="index.html">Admin</a>
-            </div>
-            <!-- /.navbar-header -->
-
-          
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        
-                    <li>
-                            <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                        </li>
-                        
-                        <li>
-                            <a href="slider.php"><i class="fa fa-sliders fa-fw"></i> Slider</a>
-                        </li>
-                        <li>
-                            <a href="nslider.php"><i class="fa fa-newspaper-o fa-fw"></i> Notice Slider</a>
-                        </li>
-                       
-                        <li>
-                            <a href="products.php"><i class="fa fa-shopping-cart fa-fw"></i> All Products</a>
-                        </li>
-                         <li>
-                            <a href="featured.php"><i class="fa fa-star fa-fw"></i> Featured Product</a>
-                        </li>
-                        <li>
-                            <a href="stats.php"><i class="fa fa-pie-chart fa-fw"></i> Stats</a>
-                        </li>
-                        <li>
-                            <a href="queries.php"><i class="fa fa-question fa-fw"></i> Queries</a>
-                        </li>
-                        <li>
-                            <a href="edit.php"><i class="fa fa-user-plus fa-fw"></i> Add / Remove Admin</a>
-                        <li>
-                            <a href="logout.php"><i class="fa fa-reply fa-fw"></i> Logout</a>
-                        </li>
-                        
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
+        <?php include("nav.php");?>
 
         <div id="page-wrapper">
             <div class="row">
@@ -165,6 +83,7 @@ Company Stats                       </div>
                                         <th>ORDERS</th>
                                         <th>CLIENTS</th>
                                         <th>SOLD</th>
+                                        <th>ACTION</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -175,9 +94,12 @@ if ($result->num_rows > 0) {
       
 ?>
                                     <tr class="">
-                                        <td><?php echo $row['orders']; ?></td>
-                                        <td><?php echo $row['clients']; ?></td>
-                                        <td><?php echo $row['sold']; ?></td>
+                                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                                        <td><input type="text" value="<?php echo $row['orders'];?>" name="orders" /></td>
+                                        <td><input type="text" value="<?php echo $row['clients'];?>" name="clients" /></td>
+                                        <td><input type="text" value="<?php echo $row['sold'];?>" name="sold" /></td>
+                                        <td><input type="submit" value="Update" class="btn btn-success" name="submit"></td>
+                                        </form>
                                     </tr>
                                      <?php
     }
@@ -186,23 +108,8 @@ if ($result->num_rows > 0) {
                                 </tbody>
                             </table>
 <br><br>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
 
-                            <div class="form-group">
-                                
-                            <input class="form-control" name="orders" placeholder="Update Orders" type="number" required>
-                            <br>
-                            <input class="form-control" name="clients" placeholder="Update Clients" type="number" required>
-
-<br>
-<input class="form-control" name="sold" placeholder="Update Sold" type="number" required>
-
-                                            
-                                        </div>
-                                        <input type="submit" value="Update" class="btn btn-success" name="submit">
-                                        
-                                        <?php echo $error; ?>
-</form>                            
+                    
                         </div>
                         <!-- /.panel-body -->
                     </div>
